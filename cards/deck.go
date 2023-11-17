c8bbe8367ca46bec0e9f8fa039913833837884d1
Lauro Santana Silva <devlauross@gmail.com>
2023-11-17 12:26:13 -0300

    write files

--- cards/deck.go
+++ cards/deck.go
@@ -1,35 +1,39 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"errors"
 	"fmt"
+	"log"
+	"os"
 )
 
-type card struct {
-	num  int
-	name string
+type Card struct {
+	Num  int
+	Name string
 }
 
-type deck []card
+type Deck []Card
 
-func initDeck() deck {
-	d := deck{}
+func initDeck() Deck {
+	d := Deck{}
 	shapes := [4]string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	for i := 1; i <= 10; i++ {
 		for _, shape := range shapes {
-			d = append(d, card{num: i, name: shape})
+			d = append(d, Card{Num: i, Name: shape})
 		}
 	}
 	return d
 }
 
-func (d *deck) Deal(size int) (deck, error) {
+func (d *Deck) Deal(size int) (Deck, error) {
 	if *d == nil {
-		return nil, errors.New("You need to init your deck with initDeck function")
+		return nil, errors.New("You need to init your Deck with initDeck function")
 	}
 	if size > len(*d) {
-		return deck{},
-			errors.New(fmt.Sprintf("The deck has a size of %v and don't support a hand size of %v", len(*d), size))
+		return Deck{},
+			errors.New(fmt.Sprintf("The Deck has a size of %v and don't support a hand size of %v", len(*d), size))
 	}
 	dx := *d
 	hand := dx[:size]
@@ -37,14 +41,35 @@ func (d *deck) Deal(size int) (deck, error) {
 	return hand, nil
 }
 
-func printCards(i int, d deck) string {
+func printCards(i int, d Deck) string {
 	if i == len(d) {
 		return ""
 	} else {
-		return fmt.Sprintf("%v of %v\n", d[i].num, d[i].name) + printCards(i+1, d)
+		return fmt.Sprintf("%v of %v\n", d[i].Num, d[i].Name) + printCards(i+1, d)
 	}
 }
 
-func (d deck) String() string {
+func (d Deck) String() string {
 	return printCards(0, d)
+}
+
+func (d Deck) SaveToFile(fileName string) error {
+	return os.WriteFile(fileName, []byte(d.String()), 0666)
+}
+
+func (d Deck) SaveToBinFile(fileName string) {
+	file, _ := os.Create(fileName)
+	defer file.Close()
+	// if er != nil {
+	// 	log.Fatalln(er)
+	// }
+	//var pi int8 = 2
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(file)
+	er := enc.Encode(d)
+	if er != nil {
+		log.Fatal(er)
+	}
+	fmt.Println(buf.Bytes())
+	//return file.Write(bin.Bytes())
 }
--- cards/main.go
+++ cards/main.go
@@ -1,22 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
-
 func main() {
-	//myDeck := initDeck()
-	var myDeck deck = nil
-	fmt.Print(myDeck)
-	fmt.Println("Dealing hand...")
-	hand, er := myDeck.Deal(0)
-
-	if er != nil {
-		log.Fatal(er)
-	}
-	fmt.Println("Your hand...")
-	fmt.Print(hand)
-	fmt.Println("Your deck...")
-	fmt.Print(myDeck)
+	myDeck := initDeck()
+	// er := myDeck.SaveToFile("my_deck.txt")
+	// if er != nil {
+	// 	log.Fatalln(er)
+	// }
+	// i, er := myDeck.SaveToBinFile("my_deck.bin")
+	// fmt.Println(i)
+	// if er != nil {
+	// 	log.Fatalln(er)
+	// }
+	myDeck.SaveToBinFile("my_deck.bin")
 }
