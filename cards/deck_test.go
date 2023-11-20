f76051f7049743227f0fd882981047fd9d4614b4
Lauro Santana Silva <devlauross@gmail.com>
2023-11-19 22:01:16 -0300

    save and read from file test

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,9 +1,40 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestInitDeck(t *testing.T) {
 	d := initDeck()
+
+	if len(d) != 40 {
+		t.Fatalf("Expected a deck with the size of 40, but recive a sized deck of: %v", len(d))
+	}
+
+	if d[0].Name != "Spades" {
+		t.Errorf("The deck created it's not correct")
+	}
+	if d[len(d)-1].Name != "Clubs" {
+		t.Errorf("The deck created it's not correct")
+	}
+}
+
+func TestSaveFileDeck(t *testing.T) {
+	fn := "_deckTest.txt"
+	defer os.Remove(fn)
+
+	d := initDeck()
+
+	er := d.SaveToFile(fn)
+	if er != nil {
+		t.Fatalf("%v", er)
+	}
+
+	d, er = ReadFromFile(fn)
+	if er != nil {
+		t.Fatalf("%v", er)
+	}
 
 	if len(d) != 40 {
 		t.Errorf("Expected a deck with the size of 40, but recive a sized deck of: %v", len(d))
