9bc6a614da4b8619e5ed6eb0dbd087e39b496899
Lauro Santana Silva <devlauross@gmail.com>
2023-11-23 23:47:17 -0300

    Emoji tests

---
+++ 07_interface/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	value := "\U0001f600"
+	fmt.Printf("%v \n", value)
+
+	value = "\U0001F92A"
+	fmt.Printf("%v \n", value)
+
+	value = "\U0001F914"
+	fmt.Printf("%v", value)
+}
