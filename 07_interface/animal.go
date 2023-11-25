a31ba34c5f3cdcc3a9a335cae6a17b09c94a98ef
Lauro Santana Silva <devlauross@gmail.com>
2023-11-25 00:29:45 -0300

    implementd animal interface

--- 07_interface/animal.go
+++ 07_interface/animal.go
@@ -3,7 +3,6 @@ package main
 type Food string
 
 type Animal interface {
-	Walk() string
 	Feed(Food) string
 	String() string
 }
--- 07_interface/main.go
+++ 07_interface/main.go
@@ -2,13 +2,36 @@ package main
 
 import "fmt"
 
+type Dog string
+
+type Cat string
+
 func main() {
-	value := "\U0001f600"
-	fmt.Printf("%v \n", value)
+	dog := Dog("\U0001F436")
+	cat := Cat("\U0001F431")
 
-	value = "\U0001F92A"
-	fmt.Printf("%v \n", value)
+	fmt.Println(Walk(dog))
+	fmt.Println(Walk(cat))
+	fmt.Println(dog.Feed("\U0001F356"))
+	fmt.Println(cat.Feed("\U0001F41F"))
+}
 
-	value = "\U0001F914"
-	fmt.Printf("%v", value)
+func Walk(a Animal) string {
+	return fmt.Sprintf("The %v is walking", a)
+}
+
+func (d Dog) Feed(f Food) string {
+	return fmt.Sprintf("The %v eats %v", d, f)
+}
+
+func (d Dog) String() string {
+	return string(d)
+}
+
+func (c Cat) Feed(f Food) string {
+	return fmt.Sprintf("The %v eats %v", c, f)
+}
+
+func (c Cat) String() string {
+	return string(c)
 }
