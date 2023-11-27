6b1aa8bbb10b896b24a0e7abebaaa741abf7b11c
Lauro Santana Silva <devlauross@gmail.com>
2023-11-26 21:57:37 -0300

    Add shape file

--- 07_interface/animal.go
+++ 07_interface/animal.go
@@ -1,8 +1,34 @@
 package main
 
+import "fmt"
+
 type Food string
 
 type Animal interface {
 	Feed(Food) string
 	String() string
+}
+
+type Dog string
+
+type Cat string
+
+func Walk(a Animal) string {
+	return fmt.Sprintf("The %v is walking", a)
+}
+
+func (d Dog) Feed(f Food) string {
+	return fmt.Sprintf("The %v eats %v", d, f)
+}
+
+func (d Dog) String() string {
+	return string(d)
+}
+
+func (c Cat) Feed(f Food) string {
+	return fmt.Sprintf("The %v eats %v", c, f)
+}
+
+func (c Cat) String() string {
+	return string(c)
 }
--- 07_interface/main.go
+++ 07_interface/main.go
@@ -1,37 +1,11 @@
 package main
 
-import "fmt"
-
-type Dog string
-
-type Cat string
-
 func main() {
-	dog := Dog("\U0001F436")
-	cat := Cat("\U0001F431")
+	// dog := Dog("\U0001F436")
+	// cat := Cat("\U0001F431")
 
-	fmt.Println(Walk(dog))
-	fmt.Println(Walk(cat))
-	fmt.Println(dog.Feed("\U0001F356"))
-	fmt.Println(cat.Feed("\U0001F41F"))
-}
-
-func Walk(a Animal) string {
-	return fmt.Sprintf("The %v is walking", a)
-}
-
-func (d Dog) Feed(f Food) string {
-	return fmt.Sprintf("The %v eats %v", d, f)
-}
-
-func (d Dog) String() string {
-	return string(d)
-}
-
-func (c Cat) Feed(f Food) string {
-	return fmt.Sprintf("The %v eats %v", c, f)
-}
-
-func (c Cat) String() string {
-	return string(c)
+	// fmt.Println(Walk(dog))
+	// fmt.Println(Walk(cat))
+	// fmt.Println(dog.Feed("\U0001F356"))
+	// fmt.Println(cat.Feed("\U0001F41F"))
 }
