98deffb755f85e88b3b22fc72c1d552071a4ed12
Lauro Santana Silva <devlauross@gmail.com>
2023-12-09 16:39:43 -0300

    Updates

---
+++ 08_http/handler/home.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	if r.Method != http.MethodGet {
+		w.Header().Add("Allow", http.MethodGet)
+		m := fmt.Sprintf("%v method not allowed", http.StatusMethodNotAllowed)
+		http.Error(w, m, http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Home"))
+}
