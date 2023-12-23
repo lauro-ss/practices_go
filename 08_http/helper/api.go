f3d49928eb364fd6a8695e07740215583fdbb9ed
Lauro Santana <devlauross@gmail.com>
2023-12-22 22:19:10 -0300

    add coments

--- 08_http/helper/api.go
+++ 08_http/helper/api.go
@@ -4,14 +4,14 @@ import (
 	"net/http"
 )
 
-type Api struct {
+type api struct {
 	mux *mux
 
 	stackServer http.Handler
 }
 
 //Pushs the middleware on top of stack server
-func (a *Api) Use(m func(http.Handler) http.Handler) {
+func (a *api) Use(m func(http.Handler) http.Handler) {
 	if a.stackServer == nil {
 		a.stackServer = m(a.mux)
 	} else {
@@ -19,26 +19,39 @@ func (a *Api) Use(m func(http.Handler) http.Handler) {
 	}
 }
 
-func NewApi() *Api {
-	return &Api{mux: newmux()}
+func NewApi() *api {
+	return &api{mux: newmux()}
 }
 
-func (a *Api) Get(pattern string, hf http.HandlerFunc) {
+//Overrides the default not found function
+func (a *api) SetNotFound(hf http.HandlerFunc) {
+	a.mux.notFound = hf
+}
+
+//The ids number default is 2
+//if you need more then two ids examle:
+//"animals/{animalId}/foods/{foodId}/type/{typeId}"
+//you need to call this function with value as 3
+func (a *api) SetIdNumber(v uint) {
+	a.mux.idsNum = v
+}
+
+func (a *api) Get(pattern string, hf http.HandlerFunc) {
 	a.mux.method(pattern, hf, http.MethodGet)
 }
 
-func (a *Api) Post(pattern string, hf http.HandlerFunc) {
+func (a *api) Post(pattern string, hf http.HandlerFunc) {
 	a.mux.method(pattern, hf, http.MethodPost)
 }
 
-func (a *Api) Put(pattern string, hf http.HandlerFunc) {
+func (a *api) Put(pattern string, hf http.HandlerFunc) {
 	a.mux.method(pattern, hf, http.MethodPut)
 }
 
-func (a *Api) Delete(pattern string, hf http.HandlerFunc) {
+func (a *api) Delete(pattern string, hf http.HandlerFunc) {
 	a.mux.method(pattern, hf, http.MethodDelete)
 }
 
-func (a *Api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (a *api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	a.stackServer.ServeHTTP(w, r)
 }
--- 08_http/helper/mux.go
+++ 08_http/helper/mux.go
@@ -8,99 +8,99 @@ import (
 
 //http method
 type method struct {
-	Func    http.HandlerFunc
-	IdNames []string
+	handlerFunc http.HandlerFunc
+	idNames     []string
 }
 
 type handler struct {
-	Methods map[string]*method
+	methods map[string]*method
 }
 
 type mux struct {
-	Handlers  map[string]*handler
+	handlers  map[string]*handler
 	getValues *regexp.Regexp   //Regex for get the values from Path
 	getIds    *regexp.Regexp   //Regex for get the Ids names from Path
-	NotFound  http.HandlerFunc //Not found method
-	IdsNum    int              //Default value is 2
+	notFound  http.HandlerFunc //Not found method
+	idsNum    uint             //Default value is 2
 }
 
 func (h *handler) newMethod(httpMethod string, hf http.HandlerFunc, ids []string) {
-	if h.Methods[httpMethod] == nil {
-		h.Methods[httpMethod] = &method{IdNames: make([]string, len(ids))}
+	if h.methods[httpMethod] == nil {
+		h.methods[httpMethod] = &method{idNames: make([]string, len(ids))}
 	}
-	h.Methods[httpMethod].Func = hf
+	h.methods[httpMethod].handlerFunc = hf
 	for i, id := range ids {
 		//Remove the braces, from {id} to only id
-		h.Methods[httpMethod].IdNames[i] = id[1 : len(id)-1]
+		h.methods[httpMethod].idNames[i] = id[1 : len(id)-1]
 	}
 }
 
 func newHandler() *handler {
 	return &handler{
-		Methods: make(map[string]*method),
+		methods: make(map[string]*method),
 	}
 }
 
 func newmux() *mux {
 	return &mux{
-		Handlers:  make(map[string]*handler),
+		handlers:  make(map[string]*handler),
 		getValues: regexp.MustCompile("[0-9]+"),
 		getIds:    regexp.MustCompile("{[A-Z-a-z]+}"),
-		NotFound:  notFound,
-		IdsNum:    2,
+		notFound:  notFound,
+		idsNum:    2,
 	}
 }
 
-func (a *mux) Get(pattern string, hf http.HandlerFunc) {
-	a.method(pattern, hf, http.MethodGet)
+func (m *mux) Get(pattern string, hf http.HandlerFunc) {
+	m.method(pattern, hf, http.MethodGet)
 }
 
-func (a *mux) Post(pattern string, hf http.HandlerFunc) {
-	a.method(pattern, hf, http.MethodPost)
+func (m *mux) Post(pattern string, hf http.HandlerFunc) {
+	m.method(pattern, hf, http.MethodPost)
 }
 
-func (a *mux) Put(pattern string, hf http.HandlerFunc) {
-	a.method(pattern, hf, http.MethodPut)
+func (m *mux) Put(pattern string, hf http.HandlerFunc) {
+	m.method(pattern, hf, http.MethodPut)
 }
 
-func (a *mux) Delete(pattern string, hf http.HandlerFunc) {
-	a.method(pattern, hf, http.MethodDelete)
+func (m *mux) Delete(pattern string, hf http.HandlerFunc) {
+	m.method(pattern, hf, http.MethodDelete)
 }
 
-func (a *mux) method(pattern string, hf http.HandlerFunc, method string) {
-	ids := a.getIds.FindAllString(pattern, a.IdsNum)
+func (m *mux) method(pattern string, hf http.HandlerFunc, method string) {
+	ids := m.getIds.FindAllString(pattern, int(m.idsNum))
 	//Replace all the custom ids for a default id name
-	pattern = a.getIds.ReplaceAllString(pattern, "{id}")
-	if a.Handlers[pattern] == nil {
-		a.Handlers[pattern] = newHandler()
+	pattern = m.getIds.ReplaceAllString(pattern, "{id}")
+	if m.handlers[pattern] == nil {
+		m.handlers[pattern] = newHandler()
 	}
-	a.Handlers[pattern].newMethod(method, hf, ids)
+	m.handlers[pattern].newMethod(method, hf, ids)
 }
 
 func (h *handler) handlerMethod(w http.ResponseWriter, r *http.Request) {
-	if h.Methods[r.Method] != nil {
-		h.Methods[r.Method].Func.ServeHTTP(w, r)
+	if h.methods[r.Method] != nil {
+		h.methods[r.Method].handlerFunc.ServeHTTP(w, r)
 		return
 	}
 	h.notAllowed(w, r)
 }
 
 func (h *handler) handlerValues(w http.ResponseWriter, r *http.Request, v []string) {
-	if h.Methods[r.Method] != nil {
+	if h.methods[r.Method] != nil {
 		r.ParseForm()
-		for i, id := range h.Methods[r.Method].IdNames {
+		for i, id := range h.methods[r.Method].idNames {
 			r.Form.Add(id, v[i])
 		}
-		h.Methods[r.Method].Func.ServeHTTP(w, r)
+		h.methods[r.Method].handlerFunc.ServeHTTP(w, r)
 		return
 	}
 	h.notAllowed(w, r)
 }
 
 func (h *handler) notAllowed(w http.ResponseWriter, r *http.Request) {
-	allowed := make([]string, len(h.Methods))
+	allowed := make([]string, len(h.methods))
 	i := 0
-	for k := range h.Methods {
+	for k := range h.methods {
 		allowed[i] = k
 		i++
 	}
@@ -110,22 +110,22 @@ func (h *handler) notAllowed(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, m, http.StatusMethodNotAllowed)
 }
 
-func (a *mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (m *mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//If the path matchs anything, the server runs the handler
-	if a.Handlers[r.URL.Path] != nil {
-		a.Handlers[r.URL.Path].handlerMethod(w, r)
+	if m.handlers[r.URL.Path] != nil {
+		m.handlers[r.URL.Path].handlerMethod(w, r)
 		return
 	}
 	//Get a map key replacing all the values with a default id name
-	key := a.getValues.ReplaceAllString(r.URL.Path, "{id}")
-	if a.Handlers[key] != nil {
+	key := m.getValues.ReplaceAllString(r.URL.Path, "{id}")
+	if m.handlers[key] != nil {
 		//Get all the values from path
-		v := a.getValues.FindAllString(r.URL.Path, a.IdsNum)
-		a.Handlers[key].handlerValues(w, r, v)
+		v := m.getValues.FindAllString(r.URL.Path, int(m.idsNum))
+		m.handlers[key].handlerValues(w, r, v)
 		return
 	}
 	//If hits here, then a not found error is delivered
-	a.NotFound(w, r)
+	m.notFound(w, r)
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
