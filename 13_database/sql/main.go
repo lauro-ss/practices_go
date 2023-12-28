a0c31e23c2be931a9ad7ffcbf11bce6d28ec6129
Lauro Santana <devlauross@gmail.com>
2023-12-27 23:28:22 -0300

    update

--- 13_database/sql/main.go
+++ 13_database/sql/main.go
@@ -1,66 +1,23 @@
 package main
 
 import (
-	"bytes"
-	"os"
+	"fmt"
+	"log"
 )
 
 func main() {
-	// db, err := sql.Open("postgres", "")
-	GetEnv("t")
-}
-
-func GetEnv(key string) string {
-	b, _ := os.ReadFile(".env")
-	i := 0
-	end := len(b) - 1
-	envs := make(map[string]string, 1)
-	for i < end {
-		//35 == #
-		if b[i] == 35 {
-			skipLine(b, &i, &end)
-		} else {
-			getEnv(envs, readLine(b, &i, &end))
-			//jumps only if the next caractere isn't #
-			if b[i] != 35 {
-				i++
-			}
-		}
+	envs, err := SourceEnv()
+	if err != nil {
+		log.Fatalln(err)
 	}
-	return ""
-}
+	//"postgres://username:password@localhost:5432/database_name"
+	url := fmt.Sprintf("postgres://%v:%v@%v:%v/%v",
+		envs["USER"], envs["PASSWORD"], envs["HOST"], envs["PORT"], envs["DATABASE"])
+	fmt.Println(url)
+	// conn, err := pgx.Connect(context.Background(), url)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// defer conn.Close(context.Background())
 
-func readLine(b []byte, i *int, end *int) []byte {
-	s := make([]byte, 20) //make
-	c := 0
-	//10 == \n
-	for b[*i] != 10 && *i < *end {
-		// comment
-		if b[*i] == 35 {
-			skipLine(b, i, end)
-			break
-		} else {
-			s[c] = b[*i]
-			*i++
-			c++
-		}
-	}
-	return s
-}
-
-func getEnv(envs map[string]string, b []byte) {
-	key := b[:bytes.IndexByte(b, byte(61))]
-	value := b[bytes.IndexByte(b, byte(61)):]
-	envs[string(key)] = string(value)
-}
-
-func skipLine(b []byte, i *int, end *int) *int {
-	for b[*i] != 10 && *i < *end {
-		*i++
-	}
-	//skipLines the \n
-	if *i != *end {
-		*i++
-	}
-	return i
 }
