bb425c8ff65f45ce8abd714527b4cdf2a5e30aa8
Lauro Santana <devlauross@gmail.com>
2023-12-29 01:10:48 -0300

    updates

--- 13_database/sql/main.go
+++ 13_database/sql/main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
@@ -17,16 +17,49 @@ func main() {
 	//"postgresql://username:password@localhost:5432/database_name"
 	v := fmt.Sprintf("postgresql://%v:%v@%v:%v/%v",
 		envs["USER"], envs["PASSWORD"], envs["HOST"], envs["PORT"], envs["DATABASE"])
-	fmt.Println(v)
-	// v := fmt.Sprintf("user=%vpassword=%vhost=%vport=%vsslmode=disablepool_max_conns=10database=%v",
-	// 	envs["USER"], envs["PASSWORD"], envs["HOST"], envs["PORT"], envs["DATABASE"])
-	// fmt.Println(v)
-	conn, err := pgx.Connect(context.Background(), returnUrl(v))
+	conn, err := pgxpool.New(context.Background(), returnUrl(v))
 	if err != nil {
 		log.Fatalln(err)
 	}
-	defer conn.Close(context.Background())
+	defer conn.Close()
+	Lines(conn)
+}
 
+func Line(c *pgxpool.Pool) {
+	var (
+		id    string
+		name  string
+		emoji string
+	)
+	err := c.QueryRow(context.Background(), "select id, name, emoji from animal;").Scan(&id, &name, &emoji)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(id, name, emoji)
+}
+
+func Lines(c *pgxpool.Pool) {
+	var (
+		id    string
+		name  string
+		emoji string
+	)
+	rows, err := c.Query(context.Background(), "select id, name, emoji from animal;")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		rows.Scan(&id, &name, &emoji)
+		fmt.Println(id, name, emoji)
+	}
+
+	if rows.Err() != nil {
+		log.Fatalln(rows.Err())
+	}
+
+	fmt.Println("All Rows")
 }
 
 func returnUrl(url string) string {
