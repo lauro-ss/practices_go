ed894a61ae54cfbe5f292e83c4c3036e38ad9bbf
Lauro Santana <devlauross@gmail.com>
2024-05-17 22:57:54 -0300

    docker-compose and env

--- 16_docker/cmd/main.go
+++ 16_docker/cmd/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"my-go-app/handler"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	db.AutoMigrate(&handler.User{})
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", handler.Index)
+	mux.HandleFunc("GET /users", handler.ListUser(db))
+	mux.HandleFunc("GET /users/create", handler.InsertUser(db))
 
-	port := ":8080"
+	port := os.Getenv("SERVER_PORT")
+	fmt.Println(port)
 
 	if err := http.ListenAndServe(port, mux); err != nil {
 		log.Fatalln(err)
--- 16_docker/handler/handler.go
+++ 16_docker/handler/handler.go
@@ -1,7 +1,33 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name string
+}
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Docker é bacana \U0001F433 \n"))
+}
+
+func ListUser(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u := User{}
+		db.First(&u)
+		w.Write([]byte(fmt.Sprint(u)))
+	}
+}
+
+func InsertUser(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user := User{Name: "Lauro"}
+		fmt.Println("Criando User")
+		db.Create(&user)
+	}
 }
